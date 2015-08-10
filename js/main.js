92b3ca90827286f844b2d7f78985ab0494d49b1d
Nick McMillan <nickm@reactive.com>
2015-08-10 12:21:28 +1000

    minor tweaks, removed spin.js

--- js/main.js
+++ js/main.js
@@ -1,70 +1,65 @@
-
 $(document).ready(function(){
-
-	
-	var currentImg = 0,
-	   	frameWidth = 640, // set the width of individual car frames
-	    frameCount = 36, // how many frames are in the sprite?
-        dragSpeed = 40, // lower = faster spin whilst dragging
-	    dragging = false,
-	    lastMousePosX = 0,
-	    $carDragArea = $('#car-drag-area'),
-		$carContainer = $('#car-container'),
-		$carImg = $('#car-img'),
-        useTransforms = $('html').hasClass('csstransforms3d');
+    
+    var currentImg = 0,
+        frameWidth = 640, // set the width of individual sprite frames
+        frameCount = 36, // how many frames are in the sprite?
+        dragSpeed = 36, // lower = faster spin whilst dragging
+        dragging = false,
+        lastMousePosX = 0,
+        $carDragArea = $('#car-drag-area'),
+        $carContainer = $('#car-container'),
+        $carImg = $('#car-img'),
+        useTransforms = $('html').hasClass('csstransforms3d'); // use modernizr
         
 
+    // options for inertia
+    var intertiaAcceleration = 10, // how heavy/light the spin feels, lower = heavier
+        duration = 60; // higher = longer interia spins
+    
 
-    // variables for inertia
-    var intertiaAcceleration = 8, // how heavy/light the spin feels
-        duration = 80, // higher = longer interia spins
-        inertiaSpin = false,
+    // used throughout, not to be changed
+    var inertiaSpin = false,
         spinDirection = '',
         inputMovementSpeed,
         target = 0,
         begin = 0,
         finish = 0,
-        change = finish - begin,
+        change = 0,
         now,
         then = new Date(),
-        delta;
+        delta,
+        timestamp = 0,
+        lastInputPosition = 0;
     
     var easeIn = function(t, b, c, d) {
         return -c *(t/=d)*(t-2) + b;
     };
 
 
-  	function imageLoader(imgPath) {
+    function imageLoader(imgPath) {
 
-  		$('#loader').spin(); // this initialises the spinner preloader
-  		// in the solution, call spin(nissan.plugins.spinJs.options);
-  		// this will call the nissan default options
+        console.log('loading image');
+        
+        var img = new Image();
 
-  		var img = new Image();
+        img.src = imgPath;
 
-  		img.src = imgPath;
+        // note that this is 'one', not 'on', because we don't 
+        // need this event to fire more than once.
+        $(img).one('load', function() {
+            
+            console.log('loading finished');
 
-  		// note that this is 'one', not 'on', because we don't 
-  		// need this event to fire more than once.
-  		$(img).one('load', function() {
-  			
-  			$('#loader').data('spinner').stop();
-  			// this stops the spinner element
-  			// or you could just delete the #loader element, you know whatever,
-
-  			// now that the img has loaded, we swap out the src attribute
-  			$carImg.attr('src', imgPath);
-  			
-  		});
+            // now that the img has loaded, we swap out the src attribute
+            $carImg.attr('src', imgPath);
+            
+        });
 
 
-  	}
+    }
 
 
-    // measure the speed, and the direction of the mouse, along x axis
-    var timestamp = 0;
-    var lastInputPosition = 0;
-
+    // measure the speed and the direction of the mouse along x axis
     function measureInputMovement(e) {
         
         if (timestamp === 0) {
@@ -96,12 +91,11 @@ $(document).ready(function(){
 
         var offsetLeft;
 
-	    $carDragArea.on('mousedown touchstart', function(e) {
+        $carDragArea.on('mousedown touchstart', function(e) {
 
-	    	if (e.type !== 'touchstart') {
-	    		e.preventDefault();
-	    	}
-	    	
+            if (e.type !== 'touchstart') {
+                e.preventDefault();
+            }
             
             dragging = true;
             
@@ -110,14 +104,14 @@ $(document).ready(function(){
             lastInputPosition = 0;
             inputMovementSpeed = 0;
 
-            if (inertiaSpin) {
+            /*if (inertiaSpin) {
                 inertiaSpin = false;
                 target = 0;
                 begin = 0;
                 change = 0;
-                finish = 60;
+                finish = 0;
                 then = new Date();
-            }
+            }*/
 
 
         }).on('mousemove touchmove', function(e) {
@@ -127,7 +121,7 @@ $(document).ready(function(){
             if (e.type === 'touchmove') {
                 var e = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
             } else {
-            	e.preventDefault();
+                e.preventDefault();
             }
 
             offsetLeft = e.pageX - $carContainer[0].offsetLeft;
@@ -150,12 +144,12 @@ $(document).ready(function(){
             var $mouseUpArea = $(e.target);
 
             if ($mouseUpArea.attr('id') === 'car-drag-area' || $mouseUpArea.parents('#car-drag-area').length) {
-    	        finish = Math.abs(inputMovementSpeed);
-    	        inertia(offsetLeft);
+                finish = Math.abs(inputMovementSpeed);
+                inertia(offsetLeft);
             }
 
-	    });
-	}
+        });
+    }
 
     function doTheWhirlwind(frameWidth, currentImg) {
 
@@ -196,31 +190,33 @@ $(document).ready(function(){
     }
 
     function colourPicker() {
-    	// handle colour pickers
-    	// this is a "delegated" click handler for any <a> tags inside #colour-picker
-    	// these guys are more efficient and future-proof than adding 
-    	// click handlers to each individual <a> tag
-    	$('#colour-picker').on('click', 'a', function(e) {
-    		e.preventDefault();
+        // handle colour pickers
+        // this is a "delegated" click handler for any <a> tags inside #colour-picker
+        // these guys are more efficient and future-proof than adding 
+        // click handlers to each individual <a> tag
+        $('#colour-picker').on('click', 'a', function(e) {
+            e.preventDefault();
 
-    		// if you click a colour that's already active
-    		// return out of the function
-    		if ($(this).hasClass('active') ) {
-    			return;
-    		}
+            var $this = $(this);
 
-    		$('#colour-picker').find('a').removeClass();
-    		$(this).addClass('active');
+            // if you click a colour that's already active
+            // return out of the function
+            if ($this.hasClass('active') ) {
+                return;
+            }
 
-    		// here we grab the href of the <a> colour swatch that was clicked
-    		// and that must map to the image file name, like 'img2.png', or whatev
-    		var imgHref = this.hash.replace('#',''),
-    			imgPath = 'images/' + imgHref + '.png';
+            $('#colour-picker').find('.active').removeClass('active'); // remove existing active
+            $this.addClass('active');
 
-    		// then we send that filename as an argument into the image loader
-    		imageLoader(imgPath)
+            // here we grab the href of the <a> colour swatch that was clicked
+            // and that must map to the image file name, like 'img2.png', or whatev
+            var imgHref = this.hash.replace('#',''),
+                imgPath = 'images/' + imgHref + '.png';
 
-    	});
+            // then we send that filename as an argument into the image loader
+            imageLoader(imgPath)
+
+        });
 
     }
 
@@ -256,30 +252,30 @@ $(document).ready(function(){
     function inertia(currentMousePosX) {
 
 
-    	// Called on each frame.
-    	var onEnterFrame = function () {
+        // Called on each frame.
+        var onEnterFrame = function () {
 
-    	    begin = target;
-    	    change = finish - begin;
+            begin = target;
+            change = finish - begin;
 
-    	    // Easing into the target.
-    	    target = easeIn(1, begin, change, duration);
+            // Easing into the target.
+            target = easeIn(1, begin, change, duration);
 
-    		now = new Date();
-			delta = now - then;
+            now = new Date();
+            delta = now - then;
 
-    		if (delta > 1000/change) {
+            if (delta > 1000/change) {
                 inertiaSpin = true;
-    			
+                
                 if (dragging) { 
                     inertiaSpin = false;
                     target = 0;
                     return;
                 }
 
-    			then = now - (delta % 1000/change);
+                then = now - (delta % 1000/change);
 
-    			// inc
+                // inc
                 if (spinDirection === 'right') {
                     currentImg = --currentImg < 0 ? frameCount-1 : currentImg;
                 } else if (spinDirection === 'left') {
@@ -287,42 +283,39 @@ $(document).ready(function(){
                 } else {
                     return;
                 }
-    			
+                
                 doTheWhirlwind(frameWidth, currentImg);
 
 
-                // spin is slw enough abort the animation frame loop
-	    	    if (change < 8) {
+                // spin is slow enough abort the animation frame loop
+                if (change < intertiaAcceleration) {
                     inertiaSpin = false;
-	    	    	target = 0;
+                    target = 0;
                     begin = 0;
-	    	    	change = 0;
-	    	    	finish = 60;
-	    	    	change = 60;
+                    change = 0;
+                    finish = 60;
+                    change = 60;
                     then = new Date();
                     cancelAnimationFrame(onEnterFrame);
                     return;
-	    	    }
+                }
                 
-                ////console.log('loop')
+                //console.log('loop')
 
-    		}
+            }
 
-    		requestAnimationFrame(onEnterFrame);
+            requestAnimationFrame(onEnterFrame);
 
-    	};
+        };
 
-		onEnterFrame();
+        onEnterFrame();
     
-
-
-
     }
 
-	// call all of our functions
+    // call all of our functions
     imageLoader('images/img.png'); // here we load the first car colour img
     manageDragging();
     colourPicker();
 
-	
+    
 });
